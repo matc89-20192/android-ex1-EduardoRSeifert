944f1b9b1e08ce659249f3aca7bcab5487dd9278
EduardoRSeifert <dudaseifert@hotmail.com>
2019-08-22 13:31:44 -0300

    Exercicio1
    App exercicio1, 3 testes passados

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -1,13 +1,49 @@
 package matc89.exercicio1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static matc89.exercicio1.R.layout;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editNome;
+    private TextView labelMensagem;
+    private Button btnCumprimentar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+        btnCumprimentar = (Button) findViewById(R.id.btnCumprimentar);
+        labelMensagem = (TextView) findViewById(R.id.labelMensagem);
+        editNome = (EditText) findViewById(R.id.editNome);
+        btnCumprimentar.setText("Cumprimentar");
+        labelMensagem.setText("Alô, Mundo!");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("meutexto", labelMensagem.getText().toString());
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String meutexto = savedInstanceState.getString("meutexto");
+        labelMensagem.setText(meutexto);
+    }
+
+    public void btnCumprimentarPress(View v){
+        String typedText = "Alô, ";
+        typedText+= editNome.getText().toString();
+        typedText+="!";
+        labelMensagem.setText(typedText);
     }
 }
